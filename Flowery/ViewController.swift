65158f663d14eaf0b21e917411dbef312536a4ce
Hector Mendoza <hectormendoza@byui.edu>
2018-11-10 10:13:00 -0500

    User interface created.

--- Flowery/ViewController.swift
+++ Flowery/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    //MARK: - Camera was tapped
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        
+    }
+    
 }
 
