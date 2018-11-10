1fc60125d195147f713a665396666e1d3d53afa4
Hector Mendoza <hectormendoza@byui.edu>
2018-11-10 10:31:04 -0500

    Image Picker delegate methods created. plist updated with camera and photo library user permissions.

--- Flowery/Controller/ViewController.swift
+++ Flowery/Controller/ViewController.swift
@@ -12,16 +12,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var scrollView: UITextView!
+    
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
         
     }
     
     //MARK: - Camera was tapped
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
+        let actionSheet = UIAlertController(title: "Photo Source:", message: "Please choose a source.", preferredStyle: .actionSheet)
         
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.imagePicker.sourceType = .camera
+                self.present(self.imagePicker, animated: true, completion: nil)
+            } else {
+                print("Camera not available")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+}
+
+
+
+//MARK: - imagePicker delegate methods
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //Image was picked
     }
     
 }
